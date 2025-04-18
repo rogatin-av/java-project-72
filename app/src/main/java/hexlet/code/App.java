9ccd64a67b874ebf4539f6983cff29ad0d412396
Andrey <rogatin-av@ya.ru>
2025-04-18 12:07:32 +0300

    Add App.java

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,32 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args)  throws IOException, SQLException {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+}
