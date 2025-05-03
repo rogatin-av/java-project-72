d256f9e6d5e6458fdbd7e93125cd234b3c2bc3e2
Andrey <rogatin-av@ya.ru>
2025-05-03 16:22:15 +0300

    Add schema.sql

---
+++ app/src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS url_checks;
+DROP TABLE IF EXISTS urls;
+
+
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id BIGINT REFERENCES urls (id),
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP
+);
